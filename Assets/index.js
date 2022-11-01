96f4459295342d185a1e1b62ad19fc70bc3e3d28
RudezY <rudyyamashiro@yahoo.com>
2022-11-01 16:30:47 -0700

    HTML layout down, css to style html formatting added. Beginning to add variables for JS

--- Assets/index.js
+++ Assets/index.js
@@ -4,4 +4,59 @@ var today = moment();
 
 $("#currentDay").text(today.format("[The date today is] dddd MMM Do, YYYY [and the time is] h:mm a"));
 
+//Set up the API key
+var APIKey="6ef8902c78593fb8c183418ac1fb301d";
+
+var searchButton = $('#search-button');
+var clearButton = $("#clear-history")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
